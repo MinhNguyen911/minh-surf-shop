a6debfbc892e0bef6add04a1786b0dc499f2e4d9
Minh Nguyen <mhn3@sfu.ca>
2019-10-13 19:06:18 -0700

    Flash message partial update and add controller code for review routes

--- app.js
+++ app.js
@@ -52,6 +52,13 @@ passport.deserializeUser(User.deserializeUser());
 // title middleware (pre route)
 app.use((req,res,next) => {
     res.locals.title = 'Surf Shop';
+    // set success flash message
+    res.locals.success = req.session.success || '';
+    delete req.session.success;
+    // set error flash message
+    res.locals.error = req.session.error || '';
+    delete req.session.error;
+    //continue on to next function in middleware chain
     next();
 });
 // Mount Routes
@@ -66,12 +73,15 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => { // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
+    console.log(err);
+    req.session.error = err.message;
+    res.redirect('back');
 });
 
 
---
+++ controllers/reviews.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Post = require('../models/post');
+const Review = require('../models/review');
+
+
+
+module.exports = { // Posts Index
+// Review Create
+    async reviewCreate(req, res, next)
+    { 
+       
+    },
+// Review Update
+    async reviewUpdate(req, res, next)
+    {
+        
+    },
+    async reviewDestroy(req,res,next){
+        
+    }
+}
